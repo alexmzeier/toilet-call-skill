5f7dc0b6c73467786035c06042c2a42c922030b5
Alexander Zeier <alexander.zeier@outlook.de>
2023-10-17 15:03:21 +0200

    init and dependencies updated

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
-from mycroft import MycroftSkill, intent_file_handler
+from va import MycroftSkill, intent_file_handler
 
+from va.dataModel import UserSkillMetis
 
 class ToiletCall(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+    def __init__(self, skill_data: UserSkillMetis):
+        MycroftSkill.__init__(self, skill_data=skill_data)
 
     @intent_file_handler('call.toilet.intent')
-    def handle_call_toilet(self, message):
-        self.speak_dialog('call.toilet')
+    def handle_call_toilet(self, message, websocket):
+        self.speak('Ich habe das Personal benachrichtigt, dass Sie auf die Toilette müssen', websocket=websocket)
 
 
-def create_skill():
-    return ToiletCall()
+def create_skill(skill_data: UserSkillMetis):
+    return ToiletCall(skill_data)
 
